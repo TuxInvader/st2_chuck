a05d373da382a1cefcf373134798d1068b7f7de2
Mark Boddington <tuxinvader@users.noreply.github.com>
2016-10-11 22:21:30 +0100

    HTML Decode jokes

--- actions/icndb.py
+++ actions/icndb.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 from st2actions.runners.pythonrunner import Action
+from HTMLParser import HTMLParser
 
 class getChuck(Action):
 
@@ -12,7 +13,8 @@ class getChuck(Action):
       if response.status_code == 200:
         joke = response.json()
         if joke["type"] == "success":
-          answer = str(joke["value"]["joke"])
+          h = HTMLParser()
+          answer = h.unescape(joke["value"]["joke"])
         else:
           answer = "StackStorm doesn't orchestrate Chuck, Chuck orchestrates StackStorm"
 
