456646cf0daf9fcb95742f76b1dd5298fac122dd
Mark Boddington <tuxinvader@users.noreply.github.com>
2016-10-17 20:13:54 +0100

    support name and surname overrides

--- packs/chuck/actions/icndb.py
+++ packs/chuck/actions/icndb.py
@@ -7,9 +7,25 @@ from HTMLParser import HTMLParser
 
 class getChuck(Action):
 
-    def run(self):
+    def run(self, name, surname, explicit):
 
-      response = requests.get("http://api.icndb.com/jokes/random")
+      url = "http://api.icndb.com/jokes/random"
+      queryString = {}
+
+      if explicit is False:
+        queryString["exclude"] = "explicit"
+
+      if name is not None:
+        queryString["firstName"] = name
+
+      if surname is not None:
+        queryString["lastName"] = surname
+
+      qs = "?"
+      for param in queryString.keys():
+        qs += "{}={}&".format(param, queryString[param]) 
+
+      response = requests.get(url + qs[:-1])
       if response.status_code == 200:
         joke = response.json()
         if joke["type"] == "success":
